f95446f33d4f784c1eb6c590dd4280ba4f50954f
Splitsky <opop.neska60@gmail.com>
2022-07-21 11:46:41 +0100

    improved the imports and standardised inserting projects and groups using multiple api calls

--- interface.py
+++ interface.py
@@ -20,32 +20,37 @@ class API_interface():
         else:
             return False
 
-    def insert_dataset(self,project_name, experiment_name, dataset_in):
-        response = requests.post(url=self.path+project_name+"/"+experiment_name+"/"+dataset_in.return_name(), json=dataset_in.convertJSON())
+    def insert_dataset(self,project_name : str, experiment_name : str, dataset_in: d.Dataset):
+        response = requests.post(url=self.path+project_name+"/"+experiment_name+"/"+dataset_in.get_name(), json=dataset_in.convertJSON())
         print("Inserting single dataset")
         print("response code: " + str(response))
         print("response content: ")
         return response.json()       
 
-    def return_fulldataset(self,project_name, experiment_name, dataset_name):
+    def return_fulldataset(self,project_name: str, experiment_name : str, dataset_name: str):
         response = requests.get(url=self.path+project_name+"/"+experiment_name+"/"+dataset_name)
         print("Retrieving single dataset")
         print("response code: + str(response)")
         return response.json()
 
-    def insert_experiment(self, project_name, experiment_name, experiment: d.Experiment):
+    def insert_experiment(self, project_name : str, experiment: d.Experiment):
         # takaes in the experiment object 
         # perform multiple calls to create an experiment directory and then
         # insert datasets one by one
-        experiment_dict = experiment.return_datasets() # returns the dictionary of experiment
-
-        for i,entry in experiment_dict.items():
-            # 
-            self.insert_dataset(project_name, experiment_name, entry.get("name")) 
-
+        experiment_name = experiment.get_name()
+        response = []
+        temp = experiment.return_datasets()
+        for dataset in temp:
+            # for each dataset in experiment call API 
+            response.append(self.insert_dataset(project_name, experiment_name, dataset))
+            
         # call to initialise experiment and return structure
 
-        # loop of calls updating each dataset
+    def insert_project(self, project: d.Project):
+        response = []
+        temp = project.return_experiments()
+        for experiment in temp:
+            response.append(self.insert_experiment(project.get_name(),experiment))
     
 
 def main():
@@ -56,23 +61,22 @@ def main():
     path = "http://127.0.0.1:8000/"
     
 
-    t.create_test_file_dataset(filename)
-    dataset_in = t.load_file_dataset(filename)
     
+    #t.create_test_file_dataset(filename)
+    #dataset_in = t.load_file_dataset(filename)
+    
+    t.create_test_file_project(filename, [1,1], project_name, author_name)
+    project_in = t.load_file_project(filename)
+
+
+    print("file content")
+    print(project_in)
+
     response = requests.get(path)
     print("Checking for connection...")
     if response == 200:
         print("Connection successful")
         print(response)
-    
-    # inserting one dataset
 
-    
-    # return one dataset
-
-    print("Retrieving single dataset")
-    print("response code: " + str(response))
-    print("response content: ")
-    print(response.json())
 
 main()
