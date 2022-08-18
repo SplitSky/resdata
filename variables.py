c672804b6d7155af03cf4133c23fb8178c21cc03
Splitsky <opop.neska60@gmail.com>
2022-08-18 21:09:12 +0100

    added demo variables

---
+++ variables.py
@@ -0,0 +1,6 @@
+username = "example"
+password = "example"
+secret_key = "acd32aa328ef73aedecefa7eae8a5897f6e752cd82465eed98c73ccea81bf4ef"
+algorithm = "HS256"
+access_token_expire = 30
+cluster_id = "abcstfa"
